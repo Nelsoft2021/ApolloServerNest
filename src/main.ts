816a09d0333b86e34a71e5a821114a32e68f7943
Nelson2021 <nelsoft2000@gmail.com>
2021-10-05 16:18:50 -0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: true });
-
   await app.listen(3045);
 }
 bootstrap();
